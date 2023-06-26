11ffed0e0dc389f914d7f9b6c52fa18f0ad1680e
luka <lukabarbakadze18@gmail.com>
2023-06-26 18:12:52 +0400

    log in and sign up with supabase

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,60 +1,90 @@
 <template>
-    <div class="max-w-screen-sm mx-auto px-4 py-10">
-  
-      <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
-        <p class="text-red-500">{{ errorMsg }}</p>
-      </div>
-  
-      <form
-        class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
-      >
-        <h1 class="text-3xl text-at-light-green mb-4">Log In</h1>
-  
-        <div class="flex flex-col mb-2">
-          <label for="email" class="mb-1 text-sm text-at-light-green"
-            >Email</label
-          >
-          <input
-            type="text"
-            required
-            class="p-2 text-gray-500 focus:outline-none"
-            id="email"
-            v-model="email"
-          />
-        </div>
-  
-        <div class="flex flex-col mb-2">
-          <label for="password" class="mb-1 text-sm text-at-light-green"
-            >Password</label
-          >
-          <input
-            type="password"
-            required
-            class="p-2 text-gray-500 focus:outline-none"
-            id="password"
-            v-model="password"
-          />
-        </div>
-  
-        <button
-          type="submit"
-          class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
-        >
-          Log In
-        </button>
-  
-        <router-link class="text-sm mt-6 text-center" :to="{ name: 'Register' }">
-          Do not have an account? <span class="text-at-light-green">Register here</span>
-        </router-link>
-      </form>
+  <div class="max-w-screen-sm mx-auto px-4 py-10">
+    <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
+      <p class="text-red-500">{{ errorMsg }}</p>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref } from "vue";
-  
-  const email = ref(null);
-  const password = ref(null);
-  const errorMsg = ref(null);
-  </script>
-  
+
+    <form
+      @submit.prevent="login"
+      class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
+    >
+      <h1 class="text-3xl text-at-light-green mb-4">Log In</h1>
+
+      <div class="flex flex-col mb-2">
+        <label for="email" class="mb-1 text-sm text-at-light-green"
+          >Email</label
+        >
+        <input
+          type="text"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          id="email"
+          v-model="email"
+        />
+      </div>
+
+      <div class="flex flex-col mb-2">
+        <label for="password" class="mb-1 text-sm text-at-light-green"
+          >Password</label
+        >
+        <input
+          type="password"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          id="password"
+          v-model="password"
+        />
+      </div>
+
+      <button
+        type="submit"
+        class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
+      >
+        Log In
+      </button>
+
+      <router-link class="text-sm mt-6 text-center" :to="{ name: 'Register' }">
+        Do not have an account?
+        <span class="text-at-light-green">Register here</span>
+      </router-link>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { supabase } from "../lib/supabaseClient";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const login = async () => {
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email.value,
+      password: password.value,
+    });
+    if (error) {
+      console.log(error);
+      errorMsg.value = `Error: ${error.message}`;
+      setTimeout(() => {
+        errorMsg.value = null;
+      }, 5000);
+    }
+
+    if (data) {
+      console.log(data);
+      router.push('/');
+    }
+  } catch (error) {
+    errorMsg.value = `Error: ${error.message}`;
+    setTimeout(() => {
+      errorMsg.value = null;
+    }, 5000);
+  }
+};
+
+const email = ref(null);
+const password = ref(null);
+const errorMsg = ref(null);
+</script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="max-w-screen-sm mx-auto px-4 py-10">
-
     <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
       <p class="text-red-500">{{ errorMsg }}</p>
     </div>
 
     <form
+      @submit.prevent="register"
       class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
     >
       <h1 class="text-3xl text-at-light-green mb-4">Register</h1>
@@ -65,6 +65,37 @@
 
 <script setup>
 import { ref } from "vue";
+
+import { supabase } from "../lib/supabaseClient";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const register = async () => {
+  if (password.value === confirmPassword.value) {
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: email.value,
+        password: password.value,
+        email_confirm: true,
+      });
+      if (error) throw new Error(error);
+
+      // console.log(data);
+      router.push("/login");
+    } catch (error) {
+      errorMsg.value = error.message;
+      setTimeout(() => {
+        errorMsg.value = null;
+      }, 5000);
+    }
+    return;
+  }
+  errorMsg.value = "Error: passwords do not match";
+  setTimeout(() => {
+    errorMsg.value = null;
+  }, 5000);
+};
 
 const email = ref(null);
 const password = ref(null);
